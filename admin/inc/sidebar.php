c3e48f7cc67a33dda4ec04b9c61df5d195974cb9
Mba Thomas <angelfab631@gmail.com>
2023-01-27 01:16:32 +0100

    Create sidebar.php

---
+++ admin/inc/sidebar.php
@@ -0,0 +1,43 @@
+<input type="checkbox" name="" id="nav-toggle">
+<div class="sidebar">
+   <div class="sidebar-brand">
+      <h2><span class="fas la-shopping-bag"></span> <span>Fab&ndash;Hub</span></h2>
+   </div>
+   <div class="sidebar-menu">
+      <ul>
+         <li>
+            <a href='index.php' class="active"><span class="las la-igloo"></span>
+               <span>Dashboard</span></a>
+         </li>
+         <li>
+            <a href="products.php" ><span class="las la-clipboard-list"></span>
+               <span>Products</span></a>
+         </li>
+         <li>
+            <a href="orders.php"><span class="las la-shopping-bag"></span>
+               <span>Orders</span></a>
+         </li>
+         <li>
+            <a href="order_items.php"><span class="las la-shopping-cart"></span>
+               <span>Order Items</span></a>
+         </li>
+         <li>
+            <a href=""><span class="las la-users"></span>
+               <span>Customers</span></a>
+         </li>
+         
+         <li>
+            <a href=""><span class="las la-receipt"></span>
+               <span>Inventory</span></a>
+         </li>
+         <li>
+            <a href=""><span class="las la-user-circle"></span>
+               <span>Accounts</span></a>
+         </li>
+         <li>
+            <a href=""><span class="las la-clipboard-list"></span>
+               <span>Tasks</span></a>
+         </li>
+      </ul>
+   </div>
+</div>
