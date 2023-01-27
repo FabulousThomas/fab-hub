428fcef1ce5ed8122920f740b95f896d3ba70923
Mba Thomas <angelfab631@gmail.com>
2023-01-27 01:22:26 +0100

    Create order_items.php

---
+++ admin/order_items.php
@@ -0,0 +1,208 @@
+<?php include "./inc/head.php"; ?>
+<title>Fab&dash;Hub | Admin&dash;Orders</title>
+
+<?php
+// $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+if (isset($_GET['page']) && $_GET['page'] != "") {
+   $page = $_GET['page'];
+} else {
+   $page = 1;
+}
+
+$num_per_page = 6;
+$start_from = intval(($page) - 1) * $num_per_page;
+$prev = intval($page) - 1;
+$next = intval($page) + 1;
+
+$sql = "SELECT * FROM order_items ORDER BY order_id DESC LIMIT $start_from, $num_per_page";
+$get_items = $conn->query($sql);
+
+// $i = 1;
+
+$result = $conn->query("SELECT * FROM order_items");
+$entries = mysqli_num_rows($result);
+$totalPages = ceil($entries / $num_per_page);
+
+
+if (isset($_POST['btn-delete-order-items'])) {
+   $item_id = $_POST['item_id'];
+
+   $stmt1 = $conn->prepare("DELETE FROM order_items WHERE item_id=? LIMIT 1");
+   $stmt1->bind_param("i", $item_id);
+
+   if (!$stmt1->execute()) {
+      echo "Something went wrong";
+   } else {
+      flashMsg("message", "Deleted");
+      redirect("order_items.php?page=" . $page . "#orders");
+   }
+}
+
+?>
+
+<body>
+
+   <?php include "./inc/sidebar.php"; ?>
+
+   <div class="main-content">
+      <?php include "./inc/header.php"; ?>
+
+      <main class="">
+         <?php include "./inc/card.php"; ?>
+
+         <div class=" my-5" id="orders">
+            <div class="products">
+               <div class="card">
+                  <div class="card-header">
+                     <h4>Order Items</h4>
+                     <!-- <a class="btn-orange border-0">See all</a> -->
+                  </div>
+                  <div class="card-body">
+                     <div class="table-responsive">
+                        <table width="100%">
+                           <thead>
+                              <tr>
+                                 <td>Order Id</td>
+                                 <td>Item Id</td>
+                                 <td>Product Id</td>
+                                 <td>User Id</td>
+                                 <td>Image</td>
+                                 <td>Name</td>
+                                 <td>Price</td>
+                                 <td>Qty</td>
+                                 <td>Date</td>
+                                 <!-- <td>Action</td> -->
+                              </tr>
+                           </thead>
+                           <tbody>
+                              <?php if ($get_items) : ?>
+                                 <?php foreach ($get_items as $item) : ?>
+                                    <tr class="align-items-center">
+                                       <td><?= $item['order_id'] ?></td>
+                                       <td><?= $item['item_id'] ?></td>
+                                       <td><?= $item['product_id'] ?></td>
+                                       <td><?= $item['user_id'] ?></td>
+                                       <td><img src="../assets/img/<?= $item['product_image'] ?>"></td>
+                                       <td><?= $item['product_name'] ?></td>
+                                       <td>$ <?= $item['product_price'] ?></td>
+                                       <td><?= $item['product_qty'] ?></td>
+                                       <td><?= $item['order_date'] ?></td>
+                                       <td>
+                                          <div class="dropdown open bg-light">
+                                             <a class="p-1 dropdown-toggle" type="button" data-toggle="dropdown">
+                                                Action
+                                             </a>
+                                             <div class="dropdown-menu shadow">
+                                                <a class="dropdown-item btn-sm btn-edit-order-items" data-toggle="modals" data-target="#edit-order-items-modals">Edit</a>
+
+                                                <form method="POST">
+                                                   <input type="hidden" name="item_id" value="<?= $item['item_id']; ?>">
+                                                   <input type="submit" name="btn-delete-order-items" value="Delete" class="dropdown-item btn-sm">
+                                                </form>
+                                             </div>
+                                          </div>
+                                       </td>
+                                    </tr>
+                                 <?php endforeach; ?>
+                              <?php else : ?>
+                                 <tr>
+                                    <td colspan="3" class="text-center">
+                                       <h5 class="text-center">No product available</h5>
+                                    </td>
+                                 </tr>
+                              <?php endif; ?>
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+
+            <!-- PAGINATION -->
+            <?php if ($num_per_page) { ?>
+               <nav aria-label="Page navigation example">
+                  <ul class="pagination pagination-sm mt-5 rounded-0">
+
+                     <li class="page-item <?php if ($page <= 1) {
+                                             echo 'disabled';
+                                          } ?>">
+                        <a class="page-link" href="<?php if ($page <= 1) : ?> 
+                                                                <?= '#'; ?>
+                                                            <?php else : ?>
+                                                                <?= '?page=' . $page - 1 . '#orders'; ?>
+                                                            <?php endif; ?>" title="Previous page"><span>Previous</span>
+                        </a>
+                     </li>
+
+
+                     <li class='page-item'><a class='page-link' href='?page=1#orders' title='Page 1'>1</a></li>
+                     <li class='page-item'><a class='page-link' href='?page=2#orders' title='Page 2'>2</a></li>
+
+                     <?php if ($page >= 3) : ?>
+                        <li class='page-item'><a class='page-link' href='#' title='Page #'>...</a></li>
+                        <li class='page-item'><a class='page-link' href='<?= "?page=" . $page . '#orders'; ?>' title='Page <?= $page ?>'><?= $page; ?></a></li>
+                     <?php endif; ?>
+
+
+
+                     <li class="page-item <?php if ($page >= $totalPages) {
+                                             echo 'disabled';
+                                          } ?>">
+                        <a class="page-link" href="<?php if ($page >= $totalPages) : ?> 
+                                                                <?= '#'; ?>
+                                                            <?php else : ?>
+                                                                <?= '?page=' . $page + 1 . '#orders'; ?>
+                                                            <?php endif; ?>" title="Next page"><span>Next</span></a>
+                     </li>
+
+                  </ul>
+               </nav>
+            <?php } else {
+            } ?>
+
+         </div>
+      </main>
+
+      <footer class="text-center py-2">
+         <div class="container">
+            <p class="m-0">Developer: <a href="" class="text-dark" style="text-decoration: underline !important; color: coral !important;">Fab&ndash;Hub</a> </p>
+            <small><span style="color: coral;">Fab&ndash;Hub </span><i class="fa fa-copyright" aria-hidden="true"></i>
+               <script>
+                  var currentYear = new Date().getFullYear();
+                  document.write(currentYear);
+               </script> | All Right Reserved
+            </small>
+         </div>
+      </footer>
+   </div>
+
+   <?php
+   // require_once "./inc/modals.php";
+   include "./inc/footer.php";
+   ?>
+
+   <!-- <script>
+      $(document).ready(function () {
+         $('.btn-edit-order-items').on('click', function () {
+            $('#edit-order-items-modal').modal('show');
+
+            $tr = $(this).closest("tr");
+            var data = $tr.children("td").map(function () {
+               return $(this).text();
+            }).get();
+            // console.log(data);
+            $('#order_items_id').val(data[0]);
+            $('#order_items_cost').val(data[1]);
+            $('#order_items_status').val(data[2]);
+            $('#user_id').val(data[3]);
+            $('#user_phone').val(data[4]);
+            $('#user_email').val(data[5]);
+            $('#order_items_date').val(data[8]);
+         });
+      });
+   </script> -->
+</body>
+
+</html>
