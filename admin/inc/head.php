cc23335672f04016c21e77daa68276610b25967a
Mba Thomas <angelfab631@gmail.com>
2023-01-27 01:16:13 +0100

    Create head.php

---
+++ admin/inc/head.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require_once "../server/connection.php";
+require_once "../function/helper.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+   <!-- FONTAWESOME -->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" />
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+   <link rel="stylesheet" href="css/style.css" />
+   <!-- <link rel="stylesheet" href="../assets/css/css.css" /> -->
+
+
+</head>
+
+<body>
