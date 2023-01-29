f510b42d213259b77db04c82e594a89a5c2e50b5
Mba Thomas <angelfab631@gmail.com>
2023-01-29 23:25:48 +0100

    Create head.php

---
+++ inc/head.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP CSS only -->
+    <link rel="stylesheet" href="assets/css/bootstrap-5.2.3.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS ONLY -->
+    <link rel="stylesheet" href="assets/css/css.css">
+    
+</head>
