4a0d521e68aab8fd36afafdf4e19f781a4e77bcc
Mba Thomas <angelfab631@gmail.com>
2023-01-29 23:25:44 +0100

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,90 @@
+    <!-- FOOTER -->
+    <footer class="mt-5 my-5 w-100">
+        <div class="row container mx-auto pt-5">
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <!-- <img src="assets/img/logo.png" alt=""> -->
+                <a class="navbar-brand" href="#">
+                    <h3 class="text-capitalize mb-0"><span style="color: coral;">Fab&ndash;Hub</span></h3>
+                </a>
+                <p class="pt-3">FabHub offers the best products for the most <span>affordable prices</span></p>
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Featured</h5>
+                <ul class="text-uppercase">
+                    <li>
+                        <a href="">Men</a>
+                    </li>
+                    <li>
+                        <a href="">Women</a>
+                    </li>
+                    <li>
+                        <a href="">Boys</a>
+                    </li>
+                    <li>
+                        <a href="">Girls</a>
+                    </li>
+                    <li>
+                        <a href="">New Arrivals</a>
+                    </li>
+                    <li>
+                        <a href="">Clothes</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Contact Us</h5>
+                <div>
+                    <h6 class="text-uppercase">Address</h6>
+                    <p>123 New Street Name, City</p>
+                </div>
+                <div>
+                    <h6 class="text-uppercase">Phone</h6>
+                    <p>123 4566 874 7746</p>
+                </div>
+                <div>
+                    <h6 class="text-uppercase">Email</h6>
+                    <p>info@fabhub@gmail.com</p>
+                </div>
+            </div>
+            <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+                <h5 class="pb-2">Instagram</h5>
+                <div class="row">
+                    <img src="assets/img/featured-2.webp" alt="" class="img-fluid w-25 h-100 m-2">
+                    <img src="assets/img/featured-4.jpg" alt="" class="img-fluid w-25 h-100 m-2">
+                    <img src="assets/img/featured-3.jpg" alt="" class="img-fluid w-25 h-100 m-2">
+                    <img src="assets/img/featured-5.png" alt="" class="img-fluid w-25 h-100 m-2">
+                    <img src="assets/img/featured-1.JPG" alt="" class="img-fluid w-25 h-100 m-2">
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright mt-5">
+            <div class="row container mx-auto">
+
+                <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+                    <img src="assets/img/visa-card.png" class="img-fluid w-25" alt="pay">
+                </div>
+
+                <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-whatsapp"></i></a>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-4 text-nowrap">
+                    <p><span style="color: coral;">Fab&ndash;Hub </span>&copy;
+                        <script>
+                            var currentYear = new Date().getFullYear();
+                            document.write(currentYear);
+                        </script> All Right Reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="assets/js/jquery-3.6.1.min.js"></script>
+    <script src="assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+    <script src="assets/js/custom.js"></script>
