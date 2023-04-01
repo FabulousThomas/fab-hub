b82fb50d9d6bfee63304006c166c8ed94452e6a4
Mba Thomas <angelfab631@gmail.com>
2023-04-01 19:34:08 +0200

    Create stores.php

---
+++ stores.php
@@ -0,0 +1,127 @@
+<?php include "inc/head.php" ?>
+<title>Fab&ndash;Hub</title>
+
+<body>
+   <?php include "inc/nav.php" ?>
+
+   <!-- HOME -->
+   <section id="store" class="carousel-image-2 d-none">
+      <div class="container">
+         <h5 class="text-uppercase">your store owners</h5>
+         <h1 class="text-capitalize"><span>Your favourite </span>store owners</h1>
+         <p class="text-capitalize">amazing products for the most <span>affordable prices</span></p>
+         <!-- <a href="#" class="">Shop Now</a> -->
+      </div>
+   </section>
+
+
+   <section class="stores py-5 mt-5">
+      <div class="container mt-5">
+         <!-- <h2 class="text-center text-uppercase">Your favourite Stores</h2>
+         <hr class="mx-auto"> -->
+         <!-- <hr class="w-100"> -->
+         <div class="border-bottom d-flex align-items-center justify-content-between">
+            <p class="mb-0">Stores</p>
+            <form action="" method="POST">
+               
+            </form>
+         </div>
+         <div class="row mx-0 mt-5">
+            <div class="col-lg-4 col-md-6 col-12">
+               <div class="card rounded-0 mb-3">
+                  <div class="row">
+                     <div class="px-4 py-3">
+                        <h4 class="text-capitalize store-name" id="dash">Jendore supper store</h4>
+                        <div id="dash">
+                           <p class="mb-0"><span class="fw-bold">Category: </span><small>amet consectetur</small></p>
+                           <p class="mb-0"><span class="fw-bold">Address: </span><small>Block 360, New York street, Miami, California</small></p>
+                        </div>
+                        <a href="#" class="stretched-link btn-orange-outline">Visit Store</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+               <div class="card rounded-0 mb-3">
+                  <div class="row">
+                     <div class="px-4 py-3">
+                        <h4 class="text-capitalize store-name" id="dash">Jendore supper store</h4>
+                        <div id="dash">
+                           <p class="mb-0"><span class="fw-bold">Category: </span><small>amet consectetur</small></p>
+                           <p class="mb-0"><span class="fw-bold">Address: </span><small>Block 360, New York street, Miami, California</small></p>
+                        </div>
+                        <a href="#" class="stretched-link btn-orange-outline">Visit Store</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+               <div class="card rounded-0 mb-3">
+                  <div class="row">
+                     <div class="px-4 py-3">
+                        <h4 class="text-capitalize store-name" id="dash">Jendore supper store</h4>
+                        <div id="dash">
+                           <p class="mb-0"><span class="fw-bold">Category: </span><small>amet consectetur</small></p>
+                           <p class="mb-0"><span class="fw-bold">Address: </span><small>Block 360, New York street, Miami, California Block 360, New York street, Miami, California</small></p>
+                        </div>
+                        <a href="#" class="stretched-link btn-orange-outline">Visit Store</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12">
+               <div class="card rounded-0 mb-3">
+                  <div class="row">
+                     <div class="px-4 py-3">
+                        <h4 class="text-capitalize store-name" id="dash">Jendore supper store</h4>
+                        <div id="dash">
+                           <p class="mb-0"><span class="fw-bold">Category: </span><small>amet consectetur</small></p>
+                           <p class="mb-0"><span class="fw-bold">Address: </span><small>Block 360, New York street, Miami, California Block 360, New York street, Miami, California</small></p>
+                        </div>
+                        <a href="#" class="stretched-link btn-orange-outline">Visit Store</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12">
+               <div class="card rounded-0 mb-3">
+                  <div class="row">
+                     <div class="px-4 py-3">
+                        <h4 class="text-capitalize store-name" id="dash">Jendore supper store</h4>
+                        <div id="dash">
+                           <p class="mb-0"><span class="fw-bold">Category: </span><small>amet consectetur</small></p>
+                           <p class="mb-0"><span class="fw-bold">Address: </span><small>Block 360, New York street, Miami, California Block 360, New York street, Miami, California</small></p>
+                        </div>
+                        <a href="#" class="stretched-link btn-orange-outline">Visit Store</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12">
+               <div class="card rounded-0 mb-3">
+                  <div class="row">
+                     <div class="px-4 py-3">
+                        <h4 class="text-capitalize store-name" id="dash">Jendore supper store</h4>
+                        <div id="dash">
+                           <p class="mb-0"><span class="fw-bold">Category: </span><small>amet consectetur</small></p>
+                           <p class="mb-0"><span class="fw-bold">Address: </span><small>Block 360, New York street, Miami, California Block 360, New York street, Miami, California</small></p>
+                        </div>
+                        <a href="#" class="stretched-link btn-orange-outline">Visit Store</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+
+
+   <?php include "inc/footer.php" ?>
+
+</body>
+
+</html>
