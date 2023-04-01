f635438fd82e6aad49d5903d4f2954cceb7f323f
Mba Thomas <angelfab631@gmail.com>
2023-04-01 19:33:45 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php require './inc/head.php'; ?>
+<title>Fab&ndash;Hub | Contact Us</title>
+
+
+<body>
+    <!-- NAVBAR -->
+    <?php require './inc/nav.php'; ?>
+
+    <!-- CONTACT -->
+    <section class="my-5 py-5">
+        <div class="container mt-5 pt-5 text-center">
+            <h2 class="font-weight-bold">Contact Us</h2>
+            <hr class="mx-auto">
+
+            <div class="contact container">
+
+                <p>Phone Number: <span>+2348 888 8888 888</span></p>
+                <p>Email Address: <span>info@fabhub@gmail.com</span></p>
+                <p>We work 24/7 to give you the best experience you deserve</p>
+
+            </div>
+        </div>
+
+
+    </section>
+
+
+
+    <!-- FOOTER -->
+    <?php require './inc/footer.php'; ?>
+    <!-- JavaScript Bundle with Popper -->
+    <!-- <script src="assets/js/jquery-3.6.1.min.js"></script> -->
+    <script src="assets/js/bootstrap-5.2.3.bundle.min.js"></script>
+</body>
+
+</html>
